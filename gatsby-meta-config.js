e3f946c71d843b8a044f230eb5cfbece2853cf2e
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-04-15 19:08:19 +0900

    [edit] meta-data

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Awesomeo184 Blog`,
+  description: `awesomeo184 dev blog`,
+  author: `awesomeo184`,
+  introduction: `개발하자`,
+  siteUrl: `https://awesomeo184.netlify.app`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `awesomeo184`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -16,13 +16,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'awesomeo184/AwesomeoBlog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'awesomeo184',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
